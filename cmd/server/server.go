fbcdb95164194059e66d4abd7dff1ec1d93e1b23
Luccas Pio <luccaspio890@gmail.com>
2023-07-27 19:24:24 -0300

    criandos resolvers e a tabela de dados

---
+++ cmd/server/server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/DevPio/gographql/graph"
+	"github.com/DevPio/gographql/internal/database"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	fmt.Println(err)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	categoryDb := database.NewCategory(db)
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{CategoryDb: categoryDb}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
